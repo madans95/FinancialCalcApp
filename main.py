eb8094f4faeb1b1a8fb2ef75c8730ced14bbaa4a
akhiill540@gmail.com <akhiill540@gmail.com>
2023-07-16 12:07:20 +0530

    added input part

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+salary = print(int(input("enter your salaray: ")))
+houserent = print(int(input("enter your houserent: ")))
+groceries = print(int(input("enter your groceries: ")))
+emi = print(float(input("enter your emi: ")))
+petrolexpenses = print(int(input("enter your petrol expenses: ")))
 
